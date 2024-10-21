5504114663a215e4d9b6885cd061498130b8ef09
ramyaraa <rastyking2003@gmail.com>
2024-10-21 14:10:31 +0300

    use condition without {}

--- src/bro_1/condition.java
+++ src/bro_1/condition.java
@@ -12,5 +12,14 @@ public class condition {
         else{
             System.out.println(sanAge+" you'r a baby coje coje coje");
         }
+
+        int adamAge = 22;
+
+        // did you know you if you have one condition you can remove {} 😁
+
+        if(adamAge >= 18)
+            System.out.println("big boy");
+        else
+            System.out.println("fuck that shit you'r so fucking youing");
     }
 }
