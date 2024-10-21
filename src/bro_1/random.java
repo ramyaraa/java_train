0ce94f8460efef69ad17fc0e2ccaee3311a539bd
ramyaraa <rastyking2003@gmail.com>
2024-10-21 13:20:08 +0300

    adding random.java

--- src/bro_1/random.java
+++ src/bro_1/random.java
@@ -1,14 +1,33 @@
 package bro_1;
 
-import java.util.Scanner;
+import java.util.Random;
 
 public class random {
     public static void main(String[] args) {
-     Scanner input = new Scanner(System.in);
-     Scanner input = new Scanner(System.in);
-        for (int j = 0; j < ; j++) {
 
-        }
+        Random rand = new Random();
+
+        // this is generate random number between [0,6]
+        int a = rand.nextInt(6)+1;
+
+        //this is generated random number between [5,10]
+        int b = rand.nextInt(4,10)+1;
+
+        //this is generated random number between [0,0.0009]
+        double c = rand.nextDouble();
+
+        // this is generated random bollean ture or false
+        boolean d = rand.nextBoolean();
+
+        //
+        long e = rand.nextLong();
+
+        System.out.println("this is random number between [0,6]: "+a);
+        System.out.println("this is random number between [5,10]: "+b);
+        System.out.println("this is random number between [0,0.9]`: "+c);
+        System.out.println("this is random true or false   : "+d);
+        System.out.println("this is random huge number because it's long: "+e);
+
 
 
 
