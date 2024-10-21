5bc89a8051de221f3cbbb7f15d7a239f2332f26e
ramyaraa <rastyking2003@gmail.com>
2024-10-21 14:06:32 +0300

    adding condition.java

---
+++ src/bro_1/condition.java
@@ -0,0 +1,16 @@
+package bro_1;
+
+public class condition {
+    public static void main(String[] args) {
+
+
+        int sanAge = 13;
+
+        if (sanAge >= 18) {
+            System.out.println(sanAge+" you'r are adult");
+        }
+        else{
+            System.out.println(sanAge+" you'r a baby coje coje coje");
+        }
+    }
+}
