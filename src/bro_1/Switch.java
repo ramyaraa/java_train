822cbcb1daf464db1a5808545b482dcb3a548fbe
ramyaraa <rastyking2003@gmail.com>
2024-10-21 14:25:15 +0300

    adding swithc.java

---
+++ src/bro_1/Switch.java
@@ -0,0 +1,24 @@
+package bro_1;
+
+public class Switch {
+    public static void main(String[] args) {
+
+        String day = "gg";
+
+        switch (day) {
+            case "saturday": System.out.println("the day is saturday");
+            break;
+            case "sunday": System.out.println("the day is sunday");
+            break;
+            case "monday": System.out.println("the day is monday");
+            break;
+            case "tusday": System.out.println("the day is tusday");
+            break;
+            case "wednesday": System.out.println("the day is wednesday");
+            break;
+            case "thursday": System.out.println("the day is thursday");
+            break;
+            default: System.out.println("the is not a day bro!!!");
+        }
+    }
+}
